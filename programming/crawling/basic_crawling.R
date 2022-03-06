1e2495fce28890aab58bbfc855ad161707dd7137
Hoya Jaeho Lee <66813492+hoyajhl@users.noreply.github.com>
2022-03-06 02:37:04 -0500

    Update basic_crawling.R

--- programming/crawling/basic_crawling.R
+++ programming/crawling/basic_crawling.R
@@ -1,29 +1,29 @@
 # 기본 크롤링 개념 및 방법
 
-url<-"https://www.bobaedream.co.kr/list?code=best"
+url<-"url주소 입력"
 a<-readLines(url,encoding="UTF-8")
 
 library(stringr)
 b<-a[str_detect(a,"<a class=\"bsubject\"")] ## 규칙 찾기
 length(b)
 
-bb<-str_extract(b,("(?<=href=).*(?=date)")) 
+bb<-str_extract(b,("(?<=href=).*(?=date)"))## 해당 속해 있는 string 반환 
 ##정규 표현식 이용해서 b에서 aaa와 bbb 사이에 있는 모든 것
 ## "(?<=aaa).*(?=bbb)"
 str_sub(bb,2) ## 앞 2개 제외
-url_list<-paste0("https://www.bobaedream.co.kr/",str_sub(bb,2),"date")
+url_list<-paste0("기본 url주소",str_sub(bb,2),"date")
 url_list
 
 ## 개별적인 url에 접근
-title_vec<-c()
-con_vec<-c()
+title_vec<-c() ## 빈 title_vec 생성
+con_vec<-c()   ## 빈 con_vec 생성 
 j<-1
 for(j in 1:length(url_list)){
   b<-readLines(url_list[j],encoding="UTF-8")
   title_vec[j]<-b[str_detect(b,"<title>")]
-  con_vec[j]<-b[which(str_detect(b,"<div class=\"bodyCont\""))+6]
-  ## which(str_detect(b,"<div class=\"bodyCont\"")): 해당 line만 나옴
-  cat("\n",j)
+  con_vec[j]<-b[which(str_detect(b,"<div class=\"bodyCont\""))+6] ## 내용 contents는 bodyCont
+  ## which(str_detect(b,"<div class=\"bodyCont\"")): 해당 line만 나옴 (숫자로 반환)
+  cat("\n",j) ## for loop 진행 상황 확인 
 }
 
 
